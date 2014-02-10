5d5db5034c58425961658a0d770bab1455ee589c
Quasar Jarosz <quasar@ja.rosz.org>
2014-02-09 22:31:06 -0600

    initial websocket code

--- app/hello.py
+++ app/hello.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sockets import Sockets
+import json
+from pprint import pprint
 
 app = Flask(__name__)
 sockets = Sockets(app)
@@ -16,6 +18,24 @@ def test_socket(ws):
     while True:
         message = ws.receive()
         ws.send("Well, this here is a test!")
+
+@sockets.route('/main')
+def main_socket(ws):
+    while True:
+        message = ws.receive()
+        handle_message(message, ws)
+
+
+def handle_message(message, ws):
+    print "Handling message: " + message
+    try:
+        msg = json.loads(message)
+    except:
+        print "Received message but it wasn't JSON :("
+        ws.send("error")
+    print "Message follows:"
+    pprint(msg)
+    ws.send("message handled")
 
 
 @app.route('/')
